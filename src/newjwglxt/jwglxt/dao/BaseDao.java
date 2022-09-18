5cade3e7d4a97163603fca69280cec70ed50763b
xiaruncheng <xiaruncheng@foxmail.com>
2022-09-18 16:26:18 +0800

    New jwglxt package.

--- src/newjwglxt/jwglxt/dao/BaseDao.java
+++ src/newjwglxt/jwglxt/dao/BaseDao.java
@@ -8,10 +8,10 @@ public interface BaseDao<T> {
     // 插入一条记录
     public void Insert(Connection connection, T t);
 
-    //删除一条记录
+    // 删除一条记录
     public void Delete(Connection connection, T t);
 
-    // 更新一条记录
+    // 更新一条记录，但id不能更改
     public void Update(Connection connection, T t0, T t1);
 
     // 完全匹配的id查找
--- src/newjwglxt/jwglxt/dao/XtadminDaoImpl.java
+++ src/newjwglxt/jwglxt/dao/XtadminDaoImpl.java
@@ -1,43 +1,178 @@
 package newjwglxt.jwglxt.dao;
 
-import java.sql.Connection;
+import newjwglxt.jwglxt.concrete.Xtadmin;
+
+import java.sql.*;
 import java.util.ArrayList;
 
-public class XtadminDaoImpl implements BaseDao {
+public class XtadminDaoImpl implements BaseDao<Xtadmin> {
     // TODO xtadmindao
 
     @Override
-    public void Insert(Connection connection, Object o) {
-
+    public void Insert(Connection connection, Xtadmin xtadmin) {
+        DatabaseMetaData databaseMetaData;
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = connection.prepareStatement("INSERT INTO xtadmin VALUES (?, ?, ?, ?, ?)");
+            preparedStatement.setInt(1, xtadmin.getId());
+            preparedStatement.setString(2, xtadmin.getName());
+            preparedStatement.setString(3, xtadmin.getGender());
+            preparedStatement.setString(4, xtadmin.getPw());
+            preparedStatement.setString(5, xtadmin.getContact());
+            databaseMetaData = connection.getMetaData();
+            if (preparedStatement.executeUpdate() != 0)
+                System.out.println(String.format("%s: \n%s", databaseMetaData.getURL(), preparedStatement));
+            else
+                System.out.println(String.format("%s: Failed.", databaseMetaData.getURL()));
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void Delete(Connection connection, Object o) {
-
+    public void Delete(Connection connection, Xtadmin xtadmin) {
+        DatabaseMetaData databaseMetaData;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM xtadmin WHERE id=?");
+            preparedStatement.setInt(1, xtadmin.getId());
+            databaseMetaData = connection.getMetaData();
+            if (preparedStatement.executeUpdate() != 0)
+                System.out.println(String.format("%s: \n%s", databaseMetaData.getURL(), preparedStatement));
+            else
+                System.out.println(String.format("%s: Failed.", databaseMetaData.getURL()));
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public void Update(Connection connection, Object t0, Object t1) {
-
+    public void Update(Connection connection, Xtadmin t0, Xtadmin t1) {
+        DatabaseMetaData databaseMetaData;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE xtadmin SET name=?, gender=?, pw=?, contact=? WHERE id=?");
+            preparedStatement.setString(1, t1.getName());
+            preparedStatement.setString(2, t1.getGender());
+            preparedStatement.setString(3, t1.getContact());
+            preparedStatement.setString(4, t1.getPw());
+            preparedStatement.setInt(5, t0.getId());
+            databaseMetaData = connection.getMetaData();
+            if (preparedStatement.executeUpdate() != 0)
+                System.out.println(String.format("%s: \n%s", databaseMetaData.getURL(), preparedStatement));
+            else
+                System.out.println(String.format("%s: Failed.", databaseMetaData.getURL()));
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public ArrayList SelectById(Connection connection, int id) {
-        return null;
+    public ArrayList<Xtadmin> SelectById(Connection connection, int id) {
+        DatabaseMetaData databaseMetaData;
+        ArrayList<Xtadmin> arrayList;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM xtadmin WHERE id=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);  // 为了下文让指针能移动
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            arrayList = new ArrayList<>();
+            databaseMetaData = connection.getMetaData();
+            if (resultSet.next())
+                System.out.println(String.format("%s: \n%s", databaseMetaData.getURL(), preparedStatement));
+            else
+                System.out.println(String.format("%s: Failed.", databaseMetaData.getURL()));
+            resultSet.beforeFirst();
+            while (resultSet.next()) {
+                arrayList.add(new Xtadmin(resultSet.getString("name"), resultSet.getInt("id"), resultSet.getString("pw"),
+                        resultSet.getString("gender"), resultSet.getString("contact")));
+            }
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return arrayList;
     }
 
     @Override
-    public ArrayList SelectByIdRough(Connection connection, int id) {
-        return null;
+    public ArrayList<Xtadmin> SelectByIdRough(Connection connection, int id) {
+        DatabaseMetaData databaseMetaData;
+        ArrayList<Xtadmin> arrayList;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM xtadmin WHERE id LIKE ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);  // 为了下文让指针能移动
+            preparedStatement.setString(1, "%" + id + "%");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            arrayList = new ArrayList<>();
+            databaseMetaData = connection.getMetaData();
+            if (resultSet.next())
+                System.out.println(String.format("%s: \n%s", databaseMetaData.getURL(), preparedStatement));
+            else
+                System.out.println(String.format("%s: Failed.", databaseMetaData.getURL()));
+            resultSet.beforeFirst();
+            while (resultSet.next()) {
+                arrayList.add(new Xtadmin(resultSet.getString("name"), resultSet.getInt("id"), resultSet.getString("pw"),
+                        resultSet.getString("gender"), resultSet.getString("contact")));
+            }
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return arrayList;
     }
 
     @Override
-    public ArrayList SelectByName(Connection connection, String name) {
-        return null;
+    public ArrayList<Xtadmin> SelectByName(Connection connection, String name) {
+        DatabaseMetaData databaseMetaData;
+        ArrayList<Xtadmin> arrayList;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM xtadmin WHERE name=?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);  // 为了下文让指针能移动
+            preparedStatement.setString(1, name);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            arrayList = new ArrayList<>();
+            databaseMetaData = connection.getMetaData();
+            if (resultSet.next())
+                System.out.println(String.format("%s: \n%s", databaseMetaData.getURL(), preparedStatement));
+            else
+                System.out.println(String.format("%s: Failed.", databaseMetaData.getURL()));
+            resultSet.beforeFirst();
+            while (resultSet.next()) {
+                arrayList.add(new Xtadmin(resultSet.getString("name"), resultSet.getInt("id"), resultSet.getString("pw"),
+                        resultSet.getString("gender"), resultSet.getString("contact")));
+            }
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return arrayList;
     }
 
     @Override
-    public ArrayList SelectByNameRough(Connection connection, String name) {
-        return null;
+    public ArrayList<Xtadmin> SelectByNameRough(Connection connection, String name) {
+        DatabaseMetaData databaseMetaData;
+        ArrayList<Xtadmin> arrayList;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM xtadmin WHERE name LIKE ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);  // 为了下文让指针能移动
+            preparedStatement.setString(1, "%" + name + "%");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            arrayList = new ArrayList<>();
+            databaseMetaData = connection.getMetaData();
+            if (resultSet.next())
+                System.out.println(String.format("%s: \n%s", databaseMetaData.getURL(), preparedStatement));
+            else
+                System.out.println(String.format("%s: Failed.", databaseMetaData.getURL()));
+            resultSet.beforeFirst();
+            while (resultSet.next()) {
+                arrayList.add(new Xtadmin(resultSet.getString("name"), resultSet.getInt("id"), resultSet.getString("pw"),
+                        resultSet.getString("gender"), resultSet.getString("contact")));
+            }
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return arrayList;
     }
 }
